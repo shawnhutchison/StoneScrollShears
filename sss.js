8f8fc40d66a45ed61814ba4acc87e5bfdf69fbe4
Shawn Hutchison <shutchison42@gmail.com>
2023-08-12 17:16:40 -0700

    Add game logic for playRound

--- sss.js
+++ sss.js
@@ -1,16 +1,30 @@
 
 function getComputerChoice() {
-    let choices = ["Stone", "Scroll", "Shears"];
+    let choices = ["stone", "scroll", "shears"];
     let randomChoice = Math.floor(Math.random() * choices.length);
   
     return choices[randomChoice];
 }
 
-function playRound (playerSelection, computerSelection) {
+// Write input statement here to gather player selection rather than literal implementation
+const playerSelection = "stone";
 
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    }
+    if (playerSelection === "stone") {
+        return (computerSelection === "scroll") ? "You lose! Scroll beats stone!" : "You win! Stone beats shears!";
+    } else if (playerSelection === "scroll") {
+        return (computerSelection === "shears") ? "You lose! Shears beats scroll!" : "You win! Scroll beats stone!";
+    } else if (playerSelection === "shears") {
+        return (computerSelection === "stone") ? "You lose! Stone beats shears!" : "You win! Shears beats scroll!";
+    }
 }
 
-const playerSelection = "rock";
+
+
+
 const computerSelection = getComputerChoice();
 
 
