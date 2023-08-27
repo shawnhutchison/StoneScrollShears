2d3374e8a5b9cef4cbb819a73cb4c8b1381134be
Shawn Hutchison <shutchison42@gmail.com>
2023-08-27 16:24:33 -0700

    Refactor code to handle event based interaction

--- sss.js
+++ sss.js
@@ -1,30 +1,14 @@
-let validChoices = ["stone", "scroll", "shears"];
-let playerSelection;
 let computerSelection;
-let playerSelectionUnconfirmed;
-
 
 function getComputerChoice() {
     let randomChoice = Math.floor(Math.random() * validChoices.length);
     return validChoices[randomChoice];
 }
 
-function playRound() {
-    playerSelectionUnconfirmed = true;
+function playRound(playerSelection) {
     let result;
     let roundScore = 0;
     let responses;
-
-    while (playerSelectionUnconfirmed) {
-        playerSelection = prompt("CHOOSE: STONE || SCROLL || SHEARS: ");
-        playerSelection = playerSelection.toLowerCase();
-
-        if (validChoices.includes(playerSelection)) {
-            playerSelectionUnconfirmed = false;
-        } else {
-            console.log("If you have any honor at all, you'll choose on of the valid options.")
-        }
-    }
     
     computerSelection = getComputerChoice();
 
@@ -49,27 +33,31 @@ function playRound() {
 }
 
 function game() {
-    roundCounter = 0;
+    
     gameActive = true;
-    let overallScore = 0;
-
-    while (gameActive) {
-        overallScore = overallScore + playRound();
-        
-        roundCounter++;
-        if (roundCounter > 4) {
-            gameActive = false;
-            console.log("Game over!");
-        }
-    }
+    let playerScore = 0;
+    let computerScore = 0;
+    
+    let maxScore = max(playerScore, computerScore);
 
     console.log("Overall Score: " + overallScore + " / 5");
 
-    if (overallScore > 2.5) {
-        console.log("You won! You will rest peacefully in Valhalla.");
-    } else if (overallScore < 2.5) {
-        console.log("You lost! You're denied entry to Valhalla and will roam the nether realm forever discontent.")
-    }
+    // This needs to go into check for end of game function.
+    // if (overallScore > 2.5) {
+    //     console.log("You won! You will rest peacefully in Valhalla.");
+    // } else if (overallScore < 2.5) {
+    //     console.log("You lost! You're denied entry to Valhalla and will roam the nether realm forever discontent.")
+    // }
+    
+    const btns = document.querySelectorAll('#playerChoices button');
+    
+    btns.forEach(btn => {
+        btn.addEventListener('click', () => {
+            playRound(btn.id);
+            // Update score
+            // Check for end of game
+        });
+    });
 }
 
 
